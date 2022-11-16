424f0b842bcb557872debf556ba71b48fd45119f
Vladimir Mansurov <70720123+VolodyaHoi@users.noreply.github.com>
2022-11-16 10:12:40 +0300

    Add files via upload

---
+++ ConsoleLabs/Lab11/Program.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * В матрице n*m отсортируйте столбцы в порядке возрастания.
+ */
+
+Random random = new Random();
+
+int n = random.Next(10);
+int m = random.Next(10);
+
+int[,] matrix = new int[n, m];
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        int element = random.Next(-25, 25);
+
+        matrix[i, j] = element;
+        Console.Write($"{element} ");
+    }
+    Console.Write('\n');
+}
+
+Console.WriteLine("Матрица с отсортированными столбцами:");
+
+for (int i = 0; i < n; i++)
+{
+    int unsortedLength = m;
+    while (unsortedLength --> 1)
+    {
+        for (int j = 0; j < unsortedLength; j++)
+        {
+            if (matrix[i, j] > matrix[i, j + 1])
+                (matrix[i, j], matrix[i, j + 1]) = (matrix[i, j + 1], matrix[i, j]);
+        }
+    }
+
+    for (int j = 0; j < m; j++)
+        Console.Write($"{matrix[i, j]} ");
+
+    Console.Write("\n");
+}
---
+++ ConsoleLabs/Lab14/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * Описать функцию Even(K) логического типа, возвращающую True, если целый
+параметр K является четным, и false в противном случае. С ее помощью найти количество
+четных чисел в наборе из 10 целых чисел.
+ */
+
+static bool Even(int K) //IsEven
+{
+    return K % 2 == 0;
+}
+
+Console.WriteLine("Введите 10 целых чисел");
+
+int count = 0;
+
+for (int i = 0; i < 10; i++)
+    if (Even(int.Parse(Console.ReadLine()))) count++;
+
+Console.WriteLine($"Их них чётны: {count} шт");
