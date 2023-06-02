eb59c45896ff7104d7c4abc3172bb97f8fa1cf99
Vladimir Mansurov <70720123+VolodyaHoi@users.noreply.github.com>
2023-06-02 12:11:07 +0300

    Delete ConsoleLabs directory

--- ConsoleLabs/Lab5/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿double anltFunc = Math.PI * Math.Cos(1) / 8;
-double calcFunc = 0;
-
-for (int n = 1; n < 50; n++)
-    calcFunc += (n * Math.Sin(2 * n)) / ((2 * n - 1) * (2 * n + 1));
-
-
-Console.WriteLine($"anltFunc = {anltFunc}\ncalcFunc = {calcFunc}");
-Console.WriteLine($"Расчёт произведён {(Math.Abs(anltFunc - calcFunc) < 0.1 ? "правильно" : "неправильно")}");
