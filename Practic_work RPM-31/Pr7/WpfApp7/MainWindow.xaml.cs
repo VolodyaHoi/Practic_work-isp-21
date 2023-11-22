1d0924a559b6e36d778eabce7bfff16f983d24bb
Vladimir Mansurov <70720123+VolodyaHoi@users.noreply.github.com>
2023-11-22 09:59:44 +0300

    Add files via upload

---
+++ Practic_work RPM-31/Pr7/WpfApp7/MainWindow.xaml.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Class;
+
+namespace WpfApp7
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int[] a_Arr = new int[3];
+        public int[] b_Arr = new int[3];
+        public int[] c_Arr = new int[3];
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void test(object sender, RoutedEventArgs e)
+        {
+            int val, val2;
+            bool val3;
+            int[] a = { 1, 6, 4 };
+            int[] b = { 2, 1, 4 };
+            int[] c = { 1, 6, 4 };
+
+            Triad triad = new Triad();
+
+            val = triad.Equals2(a, b);
+            val2 = triad.Equals2(a, b, c);
+            val3 = triad.twoNotTwo(c);
+
+            MessageBox.Show($"Первое - {val}, Второе - {val2}, Четность - {val3}");
+        }
+
+        private void startTime(object sender, RoutedEventArgs e)
+        {
+            int fh, fm, fs, sh, sm, ss;
+            bool isNum = int.TryParse(fH.Text, out fh);
+            bool isNum2 = int.TryParse(fM.Text, out fm);
+            bool isNum3 = int.TryParse(fS.Text, out fs);
+            bool isNum4 = int.TryParse(sH.Text, out sh);
+            bool isNum5 = int.TryParse(sM.Text, out sm);
+            bool isNum6 = int.TryParse(sS.Text, out ss);
+            if (isNum && isNum2 && isNum3 && isNum4 && isNum5 && isNum6)
+            {
+                if (fh <= 23 && fm <= 59 && fs <= 59 && sh <= 23 && sm <= 59 && ss <= 59)
+                {
+                    int[] timeQ;
+                    int[] a = { Convert.ToInt32(fH.Text), Convert.ToInt32(fM.Text), Convert.ToInt32(fS.Text) };
+                    int[] b = { Convert.ToInt32(sH.Text), Convert.ToInt32(sM.Text), Convert.ToInt32(sS.Text) };
+
+                    Triad triad = new Triad();
+
+                    timeQ = triad.getTime(a, b);
+
+                    int hour = timeQ[0];
+                    int minute = timeQ[1];
+                    int second = timeQ[2];
+
+                    MessageBox.Show($"Разница: {hour}h : {minute}m : {second}s");
+                } else { MessageBox.Show("Какой-то из аргументов превышен"); }
+            }
+            else { MessageBox.Show("Какой-то из аргументов указан неверно"); }
+        }
+
+        private void btn_Exit(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void btn_Info(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Практическая работа №7. Мансуров В. ИСП-31. Вариант №9");
+        }
+
+        private void menu_EqualTwoNumA(object sender, RoutedEventArgs e)
+        {
+            Triad triad = new Triad();
+            // a and b
+            int val = triad.Equals2(a_Arr, b_Arr);
+            if (val == 1)
+            {
+                MessageBox.Show("Первая и вторая тройка равны");
+            } else { MessageBox.Show("Первая и вторая тройка не равны"); }
+        }
+
+        private void menu_EqualThreeNum(object sender, RoutedEventArgs e)
+        {
+            Triad triad = new Triad();
+            // a and b and c
+            int val = triad.Equals2(a_Arr, b_Arr, c_Arr);
+            if (val == 1)
+            {
+                MessageBox.Show("Все тройки равны");
+            }
+            else { MessageBox.Show("Тройки не равны"); }
+        }
+
+        private void menu_EqualTwoNumB(object sender, RoutedEventArgs e)
+        {
+            Triad triad = new Triad();
+            // a and c
+            int val = triad.Equals2(a_Arr, c_Arr);
+            if (val == 1)
+            {
+                MessageBox.Show("Первая и третья тройка равны");
+            }
+            else { MessageBox.Show("Первая и третья тройка не равны"); }
+        }
+
+        private void menu_EqualTwoNumC(object sender, RoutedEventArgs e)
+        {
+            Triad triad = new Triad();
+            // b and c
+            int val = triad.Equals2(b_Arr, c_Arr);
+            if (val == 1)
+            {
+                MessageBox.Show("Вторая и третья тройка равны");
+            }
+            else { MessageBox.Show("Вторая и третья тройка не равны"); }
+        }
+
+        private void menu_Generate(object sender, RoutedEventArgs e)
+        {
+            A.Items.Clear();
+            B.Items.Clear();    
+            C.Items.Clear();
+            Random rnd = new Random();
+            for (int i = 0; i < 3; i++)
+            {
+                int value = rnd.Next(1, 5);
+                A.Items.Add(value);
+                a_Arr[i] = value;   
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                int value = rnd.Next(1, 5);
+                B.Items.Add(value);
+                b_Arr[i] = value;
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                int value = rnd.Next(1, 5);
+                C.Items.Add(value);
+                c_Arr[i] = value;
+            }
+        }
+
+        private void menu_twoNotTwoA(object sender, RoutedEventArgs e)
+        {
+            Triad triad = new Triad();
+            bool val = triad.twoNotTwo(a_Arr);
+            if (val == true)
+            {
+                MessageBox.Show("Первая тройка четная!");
+            }
+            else { MessageBox.Show("Первая тройка нечетная!"); }
+        }
+
+        private void menu_twoNotTwoB(object sender, RoutedEventArgs e)
+        {
+            Triad triad = new Triad();
+            bool val = triad.twoNotTwo(b_Arr);
+            if (val == true)
+            {
+                MessageBox.Show("Вторая тройка четная!");
+            }
+            else { MessageBox.Show("Вторая тройка нечетная!"); }
+        }
+
+        private void menu_twoNotTwoC(object sender, RoutedEventArgs e)
+        {
+            Triad triad = new Triad();
+            bool val = triad.twoNotTwo(c_Arr);
+            if (val == true)
+            {
+                MessageBox.Show("Третья тройка четная!");
+            }
+            else { MessageBox.Show("Третья тройка нечетная!"); }
+        }
+    }
+
+}
---
+++ Practic_work RPM-31/Pr7/WpfApp7/Triad.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    // Использовать класс Triad (тройка нечетных чисел). Определить производный класс 
+    // Time с полями: час, минута и секунда.Определить полный набор методов
+    // сравнения моментов времени
+    public class Time
+    {
+        private int hour;
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+        public int Second { get; set; }
+
+        public Time(int hour, int minute, int second)
+        {
+            Hour = hour;
+            Minute = minute;
+            Second = second;
+        }
+
+        
+    }
+
+    class Second : Time
+    {
+        public Second(int hour, int minute, int second):
+            base(hour, minute, second)
+        { 
+
+        }
+    }
+
+
+    
+    public class Triad
+    {
+       
+
+        public int Equals2(int[] fTriad, int[] sTriad)
+        {
+            int id = 0;
+            int equalNum = 0;
+
+            for (int i = 0; i < fTriad.Length; i++)
+            {
+                if (fTriad[i] == sTriad[i])
+                {
+                    equalNum++;
+                }
+            }
+
+            if (equalNum == 3)
+            {
+                id = 1;
+
+            }
+            else { id = 0; }
+
+            return id;
+
+        }
+
+        public int Equals2(int[] fTriad, int[] sTriad, int[] tTriad)
+        {
+            int id = 0;
+            int equalNum = 0;
+
+            for (int i = 0; i < fTriad.Length; i++)
+            {
+                if (fTriad[i] == sTriad[i] && fTriad[i] == tTriad[i] && sTriad[i] == tTriad[i])
+                {
+                    equalNum++;
+                }
+            }
+
+            if (equalNum == 3)
+            {
+                id = 1;
+
+            }
+            else { id = 0; }
+
+
+            return id;
+        }
+
+        public bool twoNotTwo(int[] Triad) 
+        {
+            bool two;
+            int count = 0;
+            for(int i = 0;  i < Triad.Length; i++)
+            {
+                if (Triad[i] % 2 == 0)
+                {
+                    count++;
+                }
+            }
+
+            if (count == 3)
+            {
+                two = true;
+            } else {  two  = false; }
+
+            return two;
+        }
+
+        public int[] getTime(int[] fTime, int[] sTime)
+        {
+
+            int timeHm, timeMm, timeSm; 
+            int[] fullTime = new int[3];
+
+            Time Ftime = new Time(fTime[0], fTime[1], fTime[2]);
+
+            Time Stime = new Time(sTime[0], sTime[1], sTime[2]);
+
+            timeHm = Ftime.Hour - Stime.Hour;
+            timeMm = Ftime.Minute - Stime.Minute;
+            timeSm = Ftime.Second - Stime.Second;
+
+            if (timeHm < 0)
+            {
+                timeHm = timeHm * -1;
+            }
+            if (timeMm < 0)
+            {
+                timeMm = timeMm * -1;
+            }
+            if (timeSm < 0)
+            {
+                timeSm = timeSm * -1;
+            }
+
+            fullTime[0] = timeHm;
+            fullTime[1] = timeMm;
+            fullTime[2] = timeSm;
+
+            return fullTime;
+        }
+
+    }
+}
