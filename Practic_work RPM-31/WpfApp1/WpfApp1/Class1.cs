9d96318a79db21230bb77cd1e3517d4a7c3db979
Vladimir Mansurov <70720123+VolodyaHoi@users.noreply.github.com>
2023-12-11 12:34:22 +0300

    Add files via upload

---
+++ Practic_work RPM-31/WpfApp1/WpfApp1/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public struct Class1
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public int NumberOfPages { get; set; }
+
+        public Class1(string title, string author, string publisher, int numberOfPages)
+        {
+            Title = title;
+            Author = author;
+            Publisher = publisher;
+            NumberOfPages = numberOfPages;
+        }
+    }
+}
---
+++ Practic_work RPM-31/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void menu_Exit(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void menu_Info(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Практическая работа №3. Мансуров В. ИСП-31. Вариант №9");
+        }
+
+        private void menu_SetFilter(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_AddBookCallback(object sender, RoutedEventArgs e)
+        {
+            Class1 o = new Class1("test", "test", "test", 1);
+
+            dg_AllBooks.Items.Add(o) ;
+        }
+    }
+}
