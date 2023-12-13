eeb5b76eebdc29e2d8eadd9f8207c3dfdc3af1af
Vladimir Mansurov <70720123+VolodyaHoi@users.noreply.github.com>
2023-12-13 10:42:58 +0300

    Add files via upload

---
+++ Practic_work RPM-31/Mansurov10/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private List<int> numbers;
+        public MainWindow()
+        {
+            InitializeComponent();
+            numbers = new List<int>();
+        }
+
+        private void btn_Exit(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void btn_Info(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Практическая работа №10. Мансуров В. ИСП-31. Вариант №9");
+        }
+
+        private void btn_Start(object sender, RoutedEventArgs e)
+        {
+            int k;
+            int sum = 0;
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                sum = sum + numbers[i];
+            }
+            k = sum / numbers.Count;
+            MessageBox.Show($"Результат: среднее арифметическое равно {k}");
+        }
+
+        private void btn_Add(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(tb_num.Text, out int num))
+            {
+                if (num > 0)
+                {
+                    MessageBox.Show("Число не соответствует условию");
+                } else
+                {
+                    numbers.Add(num);
+                    lb_num.Items.Add(num.ToString());
+                }             
+            }
+            else
+            {
+                MessageBox.Show("Неправильная форма числа!");
+            }
+        }
+
+        private void btn_Clear(object sender, RoutedEventArgs e)
+        {
+            numbers.Clear();
+            lb_num.Items.Clear();
+        }
+    }
+}
---
+++ Practic_work RPM-31/Mansurov11/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Mansurov11
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Exit(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void btn_Info(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Практическая работа №10. Мансуров В. ИСП-31. Вариант №9");
+        }
+
+        private void btn_Start(object sender, RoutedEventArgs e)
+        {
+            string string1 = tb_string1.Text;
+            string string2 = tb_string2.Text;
+
+            // регулярные выражения
+        }
+
+        private void btn_Clear(object sender, RoutedEventArgs e)
+        {
+            lb_str.Items.Clear();
+        }
+    }
+    
+}
